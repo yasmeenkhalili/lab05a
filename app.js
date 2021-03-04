5b1f3d81ccf61f545e77e5cd993ed3fface2f42c
yasmeenkhalili <jasmina201132@yahoo.com>
2021-03-04 20:03:12 +0200

    push the solution of problem 2

--- app.js
+++ app.js
@@ -27,11 +27,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+    let multiplication=[a*b,'The product of 5 and 9 is 45.'];
+    console.log(multiplication[0]);
+    console.log(multiplication[1]);
+    return multiplication;
+}multiply(5,9);
 
-}
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
